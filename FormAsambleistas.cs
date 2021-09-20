6f2c4d9c1472dfe43359489b73bd3a7bf68c5f83
TheMesias <f.mesias99@gmail.com>
2021-09-20 00:31:43 -0500

    Finished Project

--- FormAsambleistas.cs
+++ FormAsambleistas.cs
@@ -99,19 +99,19 @@ namespace Asambleistas
             switch (partido)
             {
                 case "ID12":
-                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/1/13/Izquierda_democratica_2018.png");
+                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Izquierda_democratica_2018.png/150px-Izquierda_democratica_2018.png");
                     txt_infoPartido.Text = "Es un partido político socialista democrático ecuatoriano, históricamente socialdemócrata ecuatoriano,2​ miembro de la Internacional Socialista. Su líder máximo, Rodrigo Borja Cevallos, fue presidente de Ecuador de 1988 hasta 1992. Fue la fuerza política más importante de ese país, tanto en resultados electorales como en organización partidaria, a finales de la década de 1980, y luego pasó a ser la segunda más importante en la década de 1990, luego del Partido Social Cristiano."; 
                     break;
                 case "CD 1":
-                    pbx_infoPartido.Load("https://pbs.twimg.com/profile_images/953015820669636609/9cv2czeD.jpg");
+                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/0/04/Centro_democratico_ecuador.png");
                     txt_infoPartido.Text = "Jairala creó este partido en el 2012, con el cual participó en las elecciones seccionales de 2014 en alianza con el movimiento Alianza PAIS, obteniendo la reelección para Jairala y varias alcaldías de la provincia, incluyendo Durán. En ese tiempo el partido existía como una organización que solo abarcaba la provincia del Guayas, teniendo como su número de lista 61.Ese mismo año de elecciones fueron parte de la coalición Frente UNIDOS como miembros fundadores, apoyando al gobierno de Rafael Correa.";
                     break;
                 case "MUPP":
-                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Pachakutik.svg/1200px-Pachakutik.svg.png");
+                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Pachakutik.svg/200px-Pachakutik.svg.png");
                     txt_infoPartido.Text = "Esta organización se define a sí misma como un movimiento político independiente que representan a otros grupos de poder. Principalmente compuesta por elementos de organizaciones indígenas tiene dentro de su ideario elementos como la constitución del estado plurinacional en el Ecuador que respete la interculturalidad. Inspirados en la cosmovisión indígena y declarando su oposición a cualquier forma de explotación, de opresión y de injusticia a la vez que apoyan la autodeterminación de los pueblos."; 
                     break;
                 case "PSC":
-                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Partido_Social_Cristiano_01.svg/1280px-Partido_Social_Cristiano_01.svg.png");
+                    pbx_infoPartido.Load("https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Partido_Social_Cristiano_01.svg/200px-Partido_Social_Cristiano_01.svg.png");
                     txt_infoPartido.Text = "El Partido Social Cristiano (PSC) es un partido político ecuatoriano de centroderecha.2​ Su tendencia se afirma en la defensa de la economía social de mercado,6​ además autodefiniéndose como conservadores que se inspiran en la doctrina social de la Iglesia.1​ Actualmente, la presidencia del partido queda vacante tras la renuncia de Pascual del Cioppo.7​ Ocupa la lista 6, y es el partido ecuatoriano más antiguo y tradicional en vigencia sin sufrir ningún cambio estructural desde su fundación."; 
                     break; 
             }
